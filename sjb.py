6ce5e0f86a8d0bad0cce7e7f675d95c228be6e19
umi1004 <61807521+umi1004@users.noreply.github.com>
2020-03-09 19:16:27 +0900

    Add files via upload

--- sjb.py
+++ sjb.py
@@ -18,11 +18,11 @@ async def on_message(message):
 # 임베드
   if message.content == "/도움":
     embed = discord.Embed(title="질문봇의 사용방법!", color=0x62c1cc)
-    embed.add_field(name="네이버, 네이트, 다음, 유튜브, 티빙, 왓챠, 넷플릭스, 메이플 인벤, 메이플지지", value="사이트", inline=False)
-    embed.add_field(name="헤이븐, 세계수, 크리티아스, 여로, 모라스, 에스페라, 어빌리티, 재획준비, 재획비, 앱솔추옵, 파프추옵", value="메이플", inline=False)
-    #embed.add_field(name="", value="", inline=False)
-    #embed.add_field(name="", value="", inline=False)
-    #embed.add_field(name="", value="", inline=False)
+    embed.add_field(value="사이트", name="네이버, 네이트, 다음, 유튜브, 티빙, 왓챠, 넷플릭스, 메이플 인벤, 메이플지지", inline=False)
+    embed.add_field(value="메이플", name="헤이븐, 세계수, 크리티아스, 여로, 모라스, 에스페라, 어빌리티, 재획준비, 재획비, 앱솔추옵, 파프추옵", inline=False)
+    embed.add_field(value="배그, 롤전적", name="ㅇㅇ 배그, 배그전적, 롤전적", inline=False)
+    #embed.add_field(value="", name="", inline=False)
+    #embed.add_field(value="", name="", inline=False)
     await message.channel.send("/를치고 검색해 주세요 :)", embed=embed)
 
   
