66c99992d400408e027ceaa71d77b2e24682b9ba
umi1004 <61807521+umi1004@users.noreply.github.com>
2020-03-09 20:31:22 +0900

    Add files via upload

--- sjb.py
+++ sjb.py
@@ -18,7 +18,7 @@ async def on_message(message):
 # 임베드
   if message.content == "/도움":
     embed.set_thumbnail(url="https://imgur.com/RIj2AXg")
-    embed = discord.Embed(icon_url="https://imgur.com/RIj2AXg", title="질문봇의 사용방법!", color=0xD1B2FF)
+    embed = discord.Embed(title="질문봇의 사용방법!", color=0xD1B2FF)
     embed.add_field(value="사이트", name="네이버, 네이트, 다음, 유튜브, 티빙, 왓챠, 넷플릭스, 메이플 인벤, 메이플지지", inline=False)
     embed.add_field(value="메이플", name="헤이븐, 세계수, 크리티아스, 여로, 모라스, 에스페라, 어빌리티, 재획준비, 재획비, 앱솔추옵, 파프추옵", inline=False)
     embed.add_field(value="배그, 롤전적", name="ㅇㅇ 배그, 배그전적, 롤전적", inline=False)
@@ -27,7 +27,7 @@ async def on_message(message):
     await message.channel.send("/를치고 검색해 주세요 :)", embed=embed)
 
   
-
+# 채팅 질문과 답
   if message.content == "/도움말":
     await message.channel.send("각종 사이트,유튜브, 메이플 인벤,메이플지지, 롤전적, 배그전적 등등.. ")
   if message.content == "/유미":
