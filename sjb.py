6dca89ef16eec8590a3acc0bd9af686da17ad05a
umi1004 <61807521+umi1004@users.noreply.github.com>
2020-03-09 19:06:52 +0900

    Add files via upload

--- sjb.py
+++ sjb.py
@@ -17,11 +17,10 @@ async def on_message(message):
 
 # 임베드
   if message.content == "/도움":
-    await message.channel.send("/를치고 검색해 주세요 :)", embed=embed)
     embed = discord.Embed(title="질문봇의 사용방법!", description="메이플", color="d1b2ff")
     embed.set_footer(text="헤이븐, 세계수, 크리티아스, 여로, 모라스, 에스페라, 어빌리티, 재획준비, 재획비, 앱솔추옵, 파프추옵")
     embed.add_field(name="사이트", value="네이버, 네이트, 다음, 유튜브, 티빙, 왓챠, 넷플릭스, 메이플 인벤, 메이플지지", inline=False)
-    
+    await message.channel.send("/를치고 검색해 주세요 :)", embed=embed)
 
   
 
